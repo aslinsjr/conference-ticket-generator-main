fe051a187e74c01e8b7307647a97d94eff243dfb
Alexandre Lins <aslinsjr@gmail.com>
2024-12-23 12:20:06 -0300

    Functions and Styles Updates

--- scripts.js
+++ scripts.js
@@ -16,13 +16,14 @@ form.addEventListener("submit", (e) => {
     sessionStorage.setItem("photo", url)
   })
 
+
   h1.innerText = `Congrats, ${e.target.name.value} ! Your ticket is ready.`
   p.innerText = `We've emailed your ticket to ${e.target.email.value} and will send updates in the run up to the event.`
 
 
-    form.innerHTML = `<img id="ticket" src="/assets/images/pattern-ticket.svg" alt="">
+  form.innerHTML = `<img id="ticket" src="/assets/images/pattern-ticket.svg" alt="">
     <div id="info-container">
-    <div class="logo">
+    <div class="logo" id="inner-ticket">
     <img src="/assets/images/logo-mark.svg" alt="">
     <h2>Coding Conf</h2>
   </div>
