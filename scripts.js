b33f0f4968de4aef5f10bd243a6b6fc089b7c4ef
Alexandre Lins <aslinsjr@gmail.com>
2024-12-23 11:47:34 -0300

    Function Update

--- scripts.js
+++ scripts.js
@@ -3,10 +3,21 @@ const h1 = document.querySelector("h1")
 const p = document.querySelector("p")
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault()
 
-    h1.innerText = `Congrats, ${e.target.name.value} ! Your ticket is ready.`
-    p.innerText = `We've emailed your ticket to ${e.target.email.value} and will send updates in the run up to the event.`
+  const fr = new FileReader();
+
+  fr.readAsDataURL(e.target.photo.files[0]);
+
+  fr.addEventListener("load", () => {
+
+    const url = fr.result
+
+    sessionStorage.setItem("photo", url)
+  })
+
+  h1.innerText = `Congrats, ${e.target.name.value} ! Your ticket is ready.`
+  p.innerText = `We've emailed your ticket to ${e.target.email.value} and will send updates in the run up to the event.`
 
 
     form.innerHTML = `<img id="ticket" src="/assets/images/pattern-ticket.svg" alt="">
@@ -16,6 +27,8 @@ form.addEventListener("submit", (e) => {
     <h2>Coding Conf</h2>
   </div>
   <p>Jan 31, 2025 / Austin, TX</p>
+  <img src=${sessionStorage.getItem("photo")} alt="">
   </div>`
 
 })
+
